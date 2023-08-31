4a34cf93e16433581fe7a4eb2c4e37cf1a080982
yeahip <142360796+yeahip@users.noreply.github.com>
2023-08-31 17:48:24 +0800

    Create ipcheck.js

---
+++ ipcheck.js
@@ -0,0 +1,28 @@
+let url = "http://ip-api.com/json"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
+    title: "节点信息",
+    content: `${ip} | IP \n${isp} | Cor\n${emoji}${country} - ${city} | Loc`,
+    icon: "globe.asia.australia.fill"
+  }
+  $done(body);
+});
+
+function getFlagEmoji(countryCode) {
+      if (countryCode.toUpperCase() == 'TW') {
+    countryCode = 'CN'
+  }
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char => 127397 + char.charCodeAt())
+  return String.fromCodePoint(...codePoints)
+
+}
